683a7e27dc89b91a53a286df6b0cc26e4ef1d87c
Lukas Holmberg <44261196+LHolmberg@users.noreply.github.com>
2020-02-04 20:42:46 +0100

    Update newton.scm

--- newton.scm
+++ newton.scm
@@ -1,1 +1,15 @@
+(define a 1) ;;Initial guess value
+(define value 25600) ;;Value to sqrt
 
+(define (f x) (- (* x x) value)) ;;f(x) = x^2 - P which means : looking for the square root
+
+(define (fprime x) (* 2 x)) ;; f'(x)
+
+(define (Newton n)
+  (cond((= n 1) a)
+  (else (set! a (- a (/ (f a) (fprime a))))
+  (Newton (- n 1)))))
+
+(define n 12) ;;num of iterations
+
+(display (Newton n))
